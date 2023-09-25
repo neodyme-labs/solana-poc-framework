f884ced88ad52978ec78c076842db839950919fb
Sebastian Fritsch <sebastian@sebastianfritsch.com>
2023-09-26 00:07:56 +0200

    update spl_token to 4.0.0

--- src/programs.rs
+++ src/programs.rs
@@ -5,4 +5,4 @@ pub const SPL_MEMO1: &[u8] = include_bytes!("programs/spl_memo-1.0.0.so");
 
 pub const SPL_MEMO3: &[u8] = include_bytes!("programs/spl_memo-3.0.0.so");
 
-pub const SPL_TOKEN: &[u8] = include_bytes!("programs/spl_token-3.1.0.so");
+pub const SPL_TOKEN: &[u8] = include_bytes!("programs/spl_token-4.0.0.so");
